4c00fbbd23a9fa3538b25c59bdfe3068115a392e
JamilServices <jamilservicos@gmail.com>
2024-05-17 09:22:34 -0300

    chore: update structure

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 
 import {CoreModule} from "@jamilservices/sb-core-module";
 import "@styles/main.scss";
-import helloWordExample from "@components/hello-world/main.js";
+import homePage from "@components/home/main.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     if(CoreModule && CoreModule.version.split(".")[0] >= 1) {
@@ -15,10 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 "state": "simply-builder.main"
             },
             "children": [
-                ...helloWordExample(CoreModule)
+                ...homePage
             ]
         };
         CoreModule.createFromStruct({
+            parent: window.document.body,
             struct: SimplyBuilderContainerStruct
         });
     }
